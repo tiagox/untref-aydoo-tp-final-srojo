b084d89d69f06ddaa7450c497c71fe0878792718
Santiago Rojo <tiagox@gmail.com>
2014-06-21 11:20:19 -0300

    Limpieza de código que nunca se ejecuta.

--- src/untref/aydoo/Recorrido.java
+++ src/untref/aydoo/Recorrido.java
@@ -4,19 +4,10 @@ public class Recorrido {
 
 	private String origen;
 	private String destino;
-	
+
 	public Recorrido(String origen, String destino) {
 		this.origen = origen;
 		this.destino = destino;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((destino == null) ? 0 : destino.hashCode());
-		result = prime * result + ((origen == null) ? 0 : origen.hashCode());
-		return result;
 	}
 
 	@Override
@@ -28,19 +19,11 @@ public class Recorrido {
 		if (getClass() != obj.getClass())
 			return false;
 		Recorrido other = (Recorrido) obj;
-		if (destino == null) {
-			if (other.destino != null)
-				return false;
-		} else if (!destino.equals(other.destino))
+		if (!destino.equals(other.destino))
 			return false;
-		if (origen == null) {
-			if (other.origen != null)
-				return false;
-		} else if (!origen.equals(other.origen))
+		if (!origen.equals(other.origen))
 			return false;
 		return true;
 	}
-	
-	
 
 }
--- test/untref/aydoo/RecorridoTest.java
+++ test/untref/aydoo/RecorridoTest.java
@@ -8,15 +8,24 @@ public class RecorridoTest {
 	@Test
 	public void debeRecibirOrigenYDestinoEnElConstructor() {
 		Recorrido recorrido = new Recorrido("DERECHO", "RETIRO");
-		
+
 		Assert.assertNotNull(recorrido);
+	}
+
+	@Test
+	public void pruebasDeMetodoEqualsAutogenerado() {
+		Recorrido recorrido = new Recorrido("DERECHO", "RETIRO");
+
+		Assert.assertTrue(recorrido.equals(recorrido));
+		Assert.assertFalse(recorrido.equals(null));
+		Assert.assertFalse(recorrido.equals("recorrido"));
 	}
 
 	@Test
 	public void compararDosRecorridosDeberiaDevolverTrueSiTienenIgualOrigenDestino() {
 		Recorrido recorrido = new Recorrido("DERECHO", "RETIRO");
 		Recorrido otroRecorrido = new Recorrido("DERECHO", "RETIRO");
-		
+
 		Assert.assertTrue(recorrido.equals(otroRecorrido));
 	}
 
@@ -24,7 +33,7 @@ public class RecorridoTest {
 	public void compararDosRecorridosDeberiaDevolverFalseSiTienenDistintoOrigenDestino() {
 		Recorrido recorrido = new Recorrido("DERECHO", "RETIRO");
 		Recorrido otroRecorrido = new Recorrido("ADUANA", "RETIRO");
-		
+
 		Assert.assertFalse(recorrido.equals(otroRecorrido));
 	}
 
@@ -32,8 +41,8 @@ public class RecorridoTest {
 	public void compararDosRecorridosDeberiaDevolverFalseSiTienenInvertidoOrigenDestino() {
 		Recorrido recorrido = new Recorrido("DERECHO", "RETIRO");
 		Recorrido otroRecorrido = new Recorrido("RETIRO", "DERECHO");
-		
+
 		Assert.assertFalse(recorrido.equals(otroRecorrido));
 	}
-	
+
 }
