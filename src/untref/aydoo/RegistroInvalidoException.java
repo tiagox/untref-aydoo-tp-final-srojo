88c55c6e7e72575f9ee9b9b11bc5638965bdbaa1
Santiago Rojo <tiagox@gmail.com>
2014-06-21 18:56:07 -0300

    Remuevo un warning al declarar una exception.

--- src/untref/aydoo/RegistroInvalidoException.java
+++ src/untref/aydoo/RegistroInvalidoException.java
@@ -2,6 +2,8 @@ package untref.aydoo;
 
 public class RegistroInvalidoException extends Exception {
 
+	private static final long serialVersionUID = -7652891185948636645L;
+
 	public RegistroInvalidoException(String message) {
 		super(message);
 	}
